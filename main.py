5188f5d6729982a2d3e5d8be02282dc6c85ddb2a
agrishaber24 <74073173+agrishaber24@users.noreply.github.com>
2020-11-17 18:17:46 +0000

    I made a mad libs code

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Aiden Grishaber 
+Period/Core: Core 7, Cohort H
 """
+
+name=input("Enter a name: ")
+
+job=input("Enter a Job title: ")
+
+workplace=input("Enter a workplace: ")
+
+Adjective=input("Enter an advective: ")
+
+carrer_choice=input("Enter a carrer choice: ")
+
+resturant=input("Enter a resturant: ")
+
+number=input("Enter a number: ")
+
+time_measurment=input("Enter a time measurment, either hours or minutes)")
+
+food_type=input("Enter a food type: ")
+
+print(f"{name} has a Job at the local {workplace}. They {Adjective} their job but thier dream job is actually a {carrer_choice}. Their best friend jessie works as a verteranarian right around the corner. They decided to go to {resturant} together, but the wait ended up being {number} {time_measurment}. Their favorite food is {food_type} While Jesse's is cheeseburger.")
